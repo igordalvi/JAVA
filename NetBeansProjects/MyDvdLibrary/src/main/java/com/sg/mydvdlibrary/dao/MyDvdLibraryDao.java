8bc6b8107a5146c84f555d5f5853c3fce6448a88
igordalvi <88341886+igordalvi@users.noreply.github.com>
2021-11-04 17:50:26 -0400

    Add files via upload
    Final Assignment - Milestone 2 [DvdLibrary]

---
+++ NetBeansProjects/MyDvdLibrary/src/main/java/com/sg/mydvdlibrary/dao/MyDvdLibraryDao.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.mydvdlibrary.dao;
+
+import com.sg.mydvdlibrary.dto.Dvd;
+import java.util.List;
+
+/**
+ *
+ * @author igord
+ */
+public interface MyDvdLibraryDao {
+
+    /**
+     * Adds the given Dvd to the library. If there is already a dvd associated with the given
+     * dvd title, it will return that dvd object, otherwise it will return
+     * null.
+     *
+     * @param dvdTitle title with which dvd is to be associated
+     * @param dvd dvd to be added to the library
+     * @return the Dvd object previously associated with the given dvd
+     * title if it exists, null otherwise
+     * @throws MyDvdLibraryDaoException
+     */
+    Dvd addDvd(String dvdTitle, Dvd dvd) throws MyDvdLibraryDaoException;
+
+    /**
+     * Returns a List of all dvds in the library.
+     *
+     * @return List containing all dvds in the library.
+     * @throws com.sg.mydvdlibrary.dao.MyDvdLibraryDaoException
+     */
+    List<Dvd> getAllDvds() throws MyDvdLibraryDaoException;
+
+    /**
+     * Returns the dvd object associated with the given dvd title.Returns null if no such dvd exists
+     *
+     * @param dvdTitle Title of the dvd to retrieve
+     * @return the Dvd object associated with the given dvd title, null if
+     * no such dvd exists
+     * @throws com.sg.mydvdlibrary.dao.MyDvdLibraryDaoException
+     */
+    Dvd getDvd(String dvdTitle) throws MyDvdLibraryDaoException;
+
+    /**
+     * Removes from the library the dvd associated with the given title.Returns the dvd object that is being removed or null if there is no dvd associated with the given title
+     *
+     * @param dvdTitle
+     * @param dvd
+     * @return Dvd object that was removed or null if no dvd was
+     * associated with the given dvd title
+     */
+    Dvd editDvd(String dvdTitle, Dvd dvd) throws MyDvdLibraryDaoException;
+    
+    Dvd removeDvd(String dvdId) throws MyDvdLibraryDaoException;
+
+}
---
+++ NetBeansProjects/MyDvdLibrary/src/main/java/com/sg/mydvdlibrary/dao/MyDvdLibraryDaoFileImpl.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.mydvdlibrary.dao;
+
+import com.sg.mydvdlibrary.dto.Dvd;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author igord
+ */
+public class MyDvdLibraryDaoFileImpl implements MyDvdLibraryDao {
+
+    public static final String DVDLIBRARY_FILE = "dvdlibrary.txt";
+    public static final String DELIMITER = "::";
+
+    private final Map<String, Dvd> dvds = new HashMap<>();
+
+    private Dvd unmarshallDvd(String dvdAsText) {
+        String[] dvdTokens = dvdAsText.split(DELIMITER);
+
+        String dvdTitle = dvdTokens[0];
+        Dvd dvdFromFile = new Dvd(dvdTitle);
+
+        dvdFromFile.setDirectorName(dvdTokens[1]);
+        dvdFromFile.setReleaseDate(dvdTokens[2]);
+        dvdFromFile.setStudio(dvdTokens[3]);
+        dvdFromFile.setMpaaRating(dvdTokens[4]);
+        dvdFromFile.setUserNote(dvdTokens[5]);
+        return dvdFromFile;
+    }
+
+    private void loadLibrary() throws MyDvdLibraryDaoException {
+        Scanner scanner;
+        try {
+            scanner = new Scanner(new BufferedReader(new FileReader(DVDLIBRARY_FILE)));
+        } catch (FileNotFoundException e) {
+            throw new MyDvdLibraryDaoException("-_- Could not load DVD data into memory.", e);
+        }
+
+        Dvd currentDvd;
+        while (scanner.hasNextLine()) {
+            currentDvd = unmarshallDvd(scanner.nextLine());
+            dvds.put(currentDvd.getDvdTitle(), currentDvd);
+        }
+        scanner.close();
+    }
+
+    private String marshallDvd(Dvd aDvd) {
+        String dvdAsText = aDvd.getDvdTitle() + DELIMITER;
+        dvdAsText += aDvd.getDirectorName() + DELIMITER;
+        dvdAsText += aDvd.getReleaseDate() + DELIMITER;
+        dvdAsText += aDvd.getStudio() + DELIMITER;
+        dvdAsText += aDvd.getMpaaRating() + DELIMITER;
+        dvdAsText += aDvd.getUserNote();
+        return dvdAsText;
+    }
+
+    private void writeLibrary() throws MyDvdLibraryDaoException {
+        PrintWriter out;
+        try {
+            out = new PrintWriter(new FileWriter(DVDLIBRARY_FILE));
+        } catch (IOException e) {
+            throw new MyDvdLibraryDaoException("Could not save DVD data.", e);
+        }
+
+        String dvdAsText;
+        List dvdList = this.getAllDvds();
+
+        for (Object currentDvd : dvdList) {
+            dvdAsText = marshallDvd((Dvd) currentDvd);
+            out.println(dvdAsText);
+            out.flush();
+        }
+
+        out.close();
+    }
+
+    @Override
+    public Dvd addDvd(String dvdTitle, Dvd dvd) throws MyDvdLibraryDaoException {
+        loadLibrary();
+        Dvd newDvd = dvds.put(dvdTitle, dvd);
+        writeLibrary();
+        return newDvd;
+    }
+
+    @Override
+    public List<Dvd> getAllDvds() throws MyDvdLibraryDaoException {
+        loadLibrary();
+        return new ArrayList<>(dvds.values());
+    }
+
+    @Override
+    public Dvd getDvd(String dvdTitle) throws MyDvdLibraryDaoException {
+        loadLibrary();
+        return dvds.get(dvdTitle);
+    }
+
+    @Override
+    public Dvd editDvd(String dvdTitle, Dvd dvd) throws MyDvdLibraryDaoException {
+        loadLibrary();
+        Dvd editedDvd = dvds.put(dvdTitle, dvd);
+        writeLibrary();
+        return editedDvd;
+    }
+
+    @Override
+    public Dvd removeDvd(String dvdTitle) throws MyDvdLibraryDaoException {
+        loadLibrary();
+        Dvd removedDvd = dvds.remove(dvdTitle);
+        writeLibrary();
+        return removedDvd;
+    }
+
+}
